696440633d3d8451315767bdb75f6dee791e348c
jadevillavicencio <jade.villavicencio@ucsp.edu.pe>
2025-08-22 20:09:59 -0500

    Agrego proyectos 'hola mundo' y 'tiposBasicos'

---
+++ tiposBasicos/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int val1 = 10;
+    int val2 = 20;
+    int result = val1 + val2;
+    cout << "El resultado es :" << result << endl;
+    return 0;
+}
