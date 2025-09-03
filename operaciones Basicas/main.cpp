dbb04865dafaa21e9f1b1ad3d4318f1857fe62a6
jadevillavicencio <jade.villavicencio@ucsp.edu.pe>
2025-09-02 22:37:16 -0500

    Cambios en el repositorio

--- operaciones Basicas/main.cpp
+++ operaciones Basicas/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    // este es un comentario para esta línea
+    int val1 = 20;
+    int val2 = 8;
+    int suma = val1 + val2; //28
+    int resta = val1 - val2; //12
+    int mult = val1 * val2; //160
+    int div = val1 / val2; //2
+    cout << "La suma es:" << suma << endl;
+    cout << "La resta es:" << suma << endl;
+    cout << "La mult. es:" << suma << endl;
+    cout << "La div. es:" << suma << endl;
+
     return 0;
 }
